94b121848326d6a5a1e4d4bbd1af4eb7c2eb79e1
LOHITH Vella <vellalohith19@gmail.com>
2026-07-04 14:22:10 +0530

    Add formatted output program for student info

---
+++ 01_Basic_Programs/04_Formatted_Output/Formatted_Output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("************************\n");
+    printf("*     STUDENT INFO     *\n");
+    printf("************************\n");
+    printf("Name   : Lohith Vella\n");
+    printf("Branch : ECE\n");
+    printf("Year   : 4th Year\n");
+
+    return 0;
+}
