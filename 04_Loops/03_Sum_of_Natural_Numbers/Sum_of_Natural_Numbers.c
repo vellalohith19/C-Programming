60b6c4ea659938064e97f5dfce7af1474dfd0f96
LOHITH Vella <vellalohith19@gmail.com>
2026-07-05 00:01:08 +0530

    Add program to calculate sum of first N natural numbers

---
+++ 04_Loops/03_Sum_of_Natural_Numbers/Sum_of_Natural_Numbers.c
@@ -0,0 +1,20 @@
+            /* Program to find the sum of first N natural numbers */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n, sum = 0;
+
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+
+    return 0;
+}
