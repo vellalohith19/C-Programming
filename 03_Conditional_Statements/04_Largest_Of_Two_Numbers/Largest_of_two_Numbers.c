0b07ab209c20a79b3694df1b71ad1d9203c5b8e1
LOHITH Vella <vellalohith19@gmail.com>
2026-07-04 23:10:49 +0530

    Add program to find the largest of two numbers
    This program prompts the user to enter two numbers and determines which one is larger, or if they are equal.

---
+++ 03_Conditional_Statements/04_Largest_Of_Two_Numbers/Largest_of_two_Numbers.c
@@ -0,0 +1,29 @@
+               /* Program to find the largest of two numbers */
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2)
+    {
+        printf("%d is the largest number.\n", num1);
+    }
+    else if (num2 > num1)
+    {
+        printf("%d is the largest number.\n", num2);
+    }
+    else
+    {
+        printf("Both numbers are equal.\n");
+    }
+
+    return 0;
+}
