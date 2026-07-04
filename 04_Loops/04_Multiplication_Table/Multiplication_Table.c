6320448460596463a2d278654d93698b453e5d8e
LOHITH Vella <vellalohith19@gmail.com>
2026-07-05 00:03:53 +0530

    Add multiplication table program in C

---
+++ 04_Loops/04_Multiplication_Table/Multiplication_Table.c
@@ -0,0 +1,20 @@
+            /* Program to print the multiplication table of a number */
+
+#include <stdio.h>
+
+int main()
+{
+    int number, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("Multiplication Table of %d\n", number);
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", number, i, number * i);
+    }
+
+    return 0;
+}
