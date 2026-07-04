533477950a1395db31a0789858328113a68db0c4
LOHITH Vella <vellalohith19@gmail.com>
2026-07-04 14:14:53 +0530

    Add Bio_Data.c to display personal information
    This program prints basic bio data including name, course, college, and state.

---
+++ 01_Basic_Programs/03_Bio_Data/Bio_Data.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name    : Lohith Vella\n");
+    printf("Course  : B.Tech (ECE)\n");
+    printf("College : Sri Vasavi Institute of Engineering And Technology \n");
+    printf("State   : Andhra Pradesh\n");
+
+    return 0;
+}
