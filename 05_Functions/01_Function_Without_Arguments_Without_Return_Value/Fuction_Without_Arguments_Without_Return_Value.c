b2402275024f9302187fb4394d20b34078d84654
LOHITH Vella <vellalohith19@gmail.com>
2026-07-05 11:05:22 +0530

    Add function to display welcome message

---
+++ 05_Functions/01_Function_Without_Arguments_Without_Return_Value/Fuction_Without_Arguments_Without_Return_Value.c
@@ -0,0 +1,17 @@
+            /* Function without arguments and without return value */
+
+#include <stdio.h>
+
+void displayMessage();
+
+int main()
+{
+    displayMessage();
+
+    return 0;
+}
+
+void displayMessage()
+{
+    printf("Welcome to C Programming!\n");
+}
