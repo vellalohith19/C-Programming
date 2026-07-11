3dac4defa0084aaeda0b9c2fa5c2922eb846da5f
LOHITH Vella <vellalohith19@gmail.com>
2026-07-11 11:33:08 +0530

    Create a C program to write student data to a file

---
+++ 01_Create_And_Write_File/Create_And_Write_File.c
@@ -0,0 +1,26 @@
+                         /* Program to create a file and write data into it */
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+
+    fp = fopen("student.txt", "w");
+
+    if (fp == NULL)
+    {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    fprintf(fp, "Name : Lohith\n");
+    fprintf(fp, "Branch : ECE\n");
+    fprintf(fp, "Year : 3rd Year\n");
+
+    fclose(fp);
+
+    printf("Data written successfully.");
+
+    return 0;
+}
